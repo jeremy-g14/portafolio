8882dad615827fc446d3499d3bc6f69fa99f1702
jeremy-g14 <jeremygonzalez1144@gmail.com>
2024-06-18 20:48:24 -0500

    Add files via upload

--- diagrama de flujo (#10) ,,.py
+++ diagrama de flujo (#10) ,,.py
@@ -1,6 +1,6 @@
 #programa 10
 
-print("rograma que solicite dos números y determine cuál es mayor o si son iguales")
+print("programa que solicite dos números y determine cuál es mayor o si son iguales")
 print("============================================================================")
 N1=float(input("Ingrese el primer numero : "))
 N2=float(input("Ingrese el segundo numero : "))
